7eaec00fe46157aa27aa41a366bc1fc6751f5452
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2023-09-17 13:37:03 +0900

    modify top-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
 export default function Home() {
   return (
-    <main>
-      Hello, world!
+    <main className="w-full">
+      <div id="main-visual" className="md:flex justify-between m-1 p-1 bg-red-100">
+        <div className="md:w-1/2 md:h-64 flex justify-center m-1 p-1 bg-red-200">
+          <div className="flex items-center">
+            Main Visual left
+          </div>
+        </div>
+        <div className="md:w-1/2 md:h-64 flex justify-center m-1 p-1 bg-red-200">
+          <div className="flex items-center">
+            Main Visual right
+          </div>
+        </div>
+      </div>
+      <div id="content1" className="flex justify-center m-1 p-1 bg-yellow-100">
+        <div className="w-full h-64 flex justify-center items-center bg-yellow-200">
+          Content1
+        </div>
+      </div>
+      <div id="content2" className="flex justify-center m-1 p-1 bg-orange-100">
+        <div className="w-full h-64 flex justify-center items-center bg-orange-200">
+          Content2
+        </div>
+      </div>
     </main>
   )
 }
